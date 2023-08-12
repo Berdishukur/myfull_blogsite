b214965fe179390b8c2d7b3804e2ade820a3763c
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-12 21:58:59 +0500

    full blogsite

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'blogapp'
 urlpatterns = [
-    # post views
-    # path('', views.post_list, name='post_list'),
+
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.post_detail,
