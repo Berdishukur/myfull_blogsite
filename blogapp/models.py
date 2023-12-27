cb40d2c43dd82947886a872070268ed373a1956b
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-27 16:15:40 +0500

    add

--- blogapp/models.py
+++ blogapp/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
