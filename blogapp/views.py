6d6dbeff09034fef953c38e7f12245871da0ba07
Berdishukur <boboqulovberdishukur@gmail.com>
2023-12-27 16:32:58 +0500

    add

--- blogapp/views.py
+++ blogapp/views.py
@@ -5,12 +5,6 @@ from .forms import EmailPostForm, CommentForm
 from django.views.generic import ListView
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 2
-    template_name = 'blog/post/list.html'
-
 
 
 def post_detail(request, year, month, day, slug):
