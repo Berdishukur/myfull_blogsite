4623c0ab3836c44926bc5c845e860850e8626cc0
Berdishukur <boboqulovberdishukur@gmail.com>
2024-02-09 20:41:28 +0500

    add

--- blog/settings.py
+++ blog/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'matemmix@gmail.com'
 EMAIL_HOST_PASSWORD = ""
