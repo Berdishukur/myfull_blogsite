1e0a821525155dc866766683e545635008fc0ff0
Berdishukur <boboqulovberdishukur@gmail.com>
2024-02-14 16:52:11 +0500

    add

--- dars.py
+++ dars.py
@@ -1,9 +1,9 @@
 
 
 
-son1=int(input("Birinchi sonni kiriting: "))
-son2=int(input("Ikkinchi sonni kiriting: "))
+son=int(input("Sonni kiriting: "))
 
-print(f"{son1 } ning {son2}- darajasi = {son1**son2}")
-print(f"{son1 } ni {son2} ga bo'lganda  qoldig'i = {son1%son2}")
-print(f"{son1  } ni {son2} ga bo'lgandagi bo'linmasi  = {son1//son2}")
+while son<10:
+    print(son)
+    son=son+1
+
