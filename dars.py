2a7a9294da021a909f7b21f23f905359f6c52d38
Berdishukur <boboqulovberdishukur@gmail.com>
2024-02-14 13:10:01 +0500

    add

--- dars.py
+++ dars.py
@@ -4,4 +4,6 @@
 son1=int(input("Birinchi sonni kiriting: "))
 son2=int(input("Ikkinchi sonni kiriting: "))
 
-print("Nateja= ", son1%son2)
+print(f"{son1 } ning {son2}- darajasi = {son1**son2}")
+print(f"{son1 } ni {son2} ga bo'lganda  qoldig'i = {son1%son2}")
+print(f"{son1  } ni {son2} ga bo'lgandagi bo'linmasi  = {son1//son2}")
